d492611fec77078c4b09a4ef266d1ef74cc3f978
fonteBean <n9ckbean@gmail.com>
2025-07-20 21:22:27 -0300

    Modificacoes no agenteRepository e agenteController

--- controllers/agentesController.js
+++ controllers/agentesController.js
@@ -1,11 +1,46 @@
 const agentesRepository = require('../repositories/agentesRepository')
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+
+
+function getAgentes(req,res){
+  const agentes = agentesRepository.findAll();
+  res.status(200).json(agentes);
+}
+
+function getAgentById(req,res){
+  const agentId = parseInt(req.params.id);
+  const agente = agentesRepository.findById(agentId);
+  if(!agente){
+       return res.status(404).send("Agente nao encontrado");
+    }
+     res.status(200).json(agente);
+}
+
+function createAgente(req,res){
+  const {nome,cargo} = req.body
+  if(!nome || !cargo){
+    return res.status(404).send("Nome e Cargo sao obrigatorios")
+  }
+  const novoAgente ={
+        id : uuidv4(),
+         nome,
+        dataDeIncorporacao: new Date(),
+        cargo
+    }
+  agentesRepository.createAgente(novoAgente);
+  res.status(201).send(novoAgente);
+}
+
 
 function deleteAgente(req,res){
-   const agenteId = parseInt(req.params.id);
-   if(!agenteId){
-    res.status(400).send("Id do agente nao passado");
-   }
-  agentesRepository.findById(agenteId);
-  
+    const agentes = agentesRepository.findAll();
+    const agentId = parseInt(req.params.id);
+    const agentIndex = agentes.findIndex(a => a.id === agenteId);
+    
+    if(!agentIndex){
+       return res.status(400).send("Agente nao encontrado");
+    }
+  agentesRepository.deleteAgente()
+  res.status(200).send();
 }
--- repositories/agentesRepository.js
+++ repositories/agentesRepository.js
@@ -4,27 +4,12 @@ function findAll(){
   return agentes;
 }
 
-function findById(id){
-    const agent = agentes.find(a => a.id === id);
-    
-    if(!agent){
-       return res.status(404).send("Agente nao encontrado");
-    }
-    return agent
+function criarAgente(agente){
+  agentes.push(agente);
 }
 
-
-function deleteAgente(id){
-  const agente = findById(id);
-  const agentIndex = agentes.findIndex(a => a.id === agente.id);
-    
-    if(!agentIndex){
-       return res.status(404).send("Agente nao encontrado");
-    }
-    agentes.splice(agentIndex, 1)
-     res.status(204).send();
-  agentes.slice(agente,1);
-
+function deleteAgente(index){
+  agentes.splice(index, 1)
 }
 
 
